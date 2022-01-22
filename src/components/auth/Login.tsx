6374b4039cdb74094036e59a84d1890510eb3680
Katie Sallows <ktsallows@gmail.com>
2022-01-22 14:02:43 -0500

    started on app.tsx

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Login extends React.Component<{}, {}> {
+    constructor(props: any) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Login;
