30899183ec28df212f2d12da66de9d913ba6a596
Katie Sallows <ktsallows@gmail.com>
2022-01-24 14:51:13 -0500

    building register page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import APIURL from './helpers/environment'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/auth/Login';
-import Register from './components/auth/Login';
+import Register from './components/auth/Register';
+import 'bulma/css/bulma.css';
 import './App.css';
 
 function App() {
   return (
     <>
       <Router>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Link to='/register'>Register</Link>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </Router>
     </>
   );
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
-class Register extends React.Component<{}, {}> {
+type RegisterValues = {
+
+}
+
+class Register extends React.Component<{}, RegisterValues> {
     constructor(props: any) {
         super(props)
+        this.state = {
+
+        }
     }
 
     render() {
         return (
-            <div>
-
+            <div className='columns'>
+                <div className='column is-2 is-offset-5'>
+                    <h1>Register</h1>
+                    <div className='field'>
+                        <div className='control'>
+                            <label className='label' htmlFor='email'>Email</label>
+                            <input className='input' type='text' id='email' name='email' />
+                            <label className='label' htmlFor='password'>Password</label>
+                            <input className='input' type='password' id='password' name='password' />
+                            <label className='label' htmlFor='passwordMatch'>Retype Password</label>
+                            <input className='input' type='passwordMatch' id='passwordMatch' name='passwordMatch' />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
