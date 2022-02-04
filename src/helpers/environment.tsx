01b623a7463fe87169e35c7b9ff1b53b266dc7cd
Katie Sallows <ktsallows@gmail.com>
2022-02-04 16:47:12 -0500

    deploying to heroku

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case '':
-        APIURL = ''
+    case 'ks-friendsoflegends-client.herokuapp.com':
+        APIURL = 'ks-friendsoflegends-server.herokuapp.com'
 }
 
 export default APIURL
