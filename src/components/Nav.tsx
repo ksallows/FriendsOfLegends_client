0c312e7831d1bee8e0f55ac11bbc9d22ad66f79d
Katie Sallows <ktsallows@gmail.com>
2022-02-04 17:47:33 -0500

    added logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header, Button, Group } from '@mantine/core';
+import { Header, Button, Group, Image } from '@mantine/core';
+import logo from '../assets/logo.svg'
 
 interface NavState {
 
@@ -23,20 +24,26 @@ class Nav extends React.Component<NavProps, NavState> {
     render() {
         return (
             <Header sx={{ padding: 10 }} height={65}>
+
                 {this.props.sessionToken ?
                     <Group position='apart'>
                         <Group>
+                            <Image src={logo} width={300} />
                             <Button size='md' variant='gradient' component={Link} to='/search' gradient={{ from: 'orange', to: 'yellow', deg: 105 }}>Find Friends</Button>
                             <Button color='gray' size='md' component={Link} to='/editprofile'>Profile</Button>
                         </Group>
-                        <Button color='gray' size='md' onClick={this.props.clearToken}>Log Out</Button>
+                        <Button variant='subtle' color='gray' size='md' onClick={this.props.clearToken}>Log Out</Button>
                     </Group>
 
                     :
 
-                    <Group>
-                        <Button size='md' variant='gradient' component={Link} to='/register' gradient={{ from: 'orange', to: 'yellow', deg: 105 }}>Sign Up</Button>
-                        <Button color='gray' size='md' component={Link} to='/login'>Log In</Button>
+                    <Group position='apart'>
+                        <Image src={logo} width={300} />
+                        <Group>
+                            <Button size='md' variant='gradient' component={Link} to='/register' gradient={{ from: 'orange', to: 'yellow', deg: 105 }}>Sign Up</Button>
+                            <Button color='gray' size='md' component={Link} to='/login'>Log In</Button>
+                        </Group>
+
                     </Group>
                 }
             </Header>
