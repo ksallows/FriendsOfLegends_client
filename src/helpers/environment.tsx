2ff38d6bc6ccf09946dc15e849499ce2a1d67fef
Katie Sallows <ktsallows@gmail.com>
2022-02-04 17:58:44 -0500

    possible fix for api url routing wrong

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'ks-friendsoflegends-client.herokuapp.com':
-        APIURL = 'ks-friendsoflegends-server.herokuapp.com'
+    case 'https://ks-friendsoflegends-client.herokuapp.com':
+        APIURL = 'https://ks-friendsoflegends-server.herokuapp.com'
 }
 
 export default APIURL
