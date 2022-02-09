a63d70bcb2db6d5f98d17c104905807c331eb30c
Katie Sallows <ktsallows@gmail.com>
2022-02-08 21:01:52 -0500

    SEARCH IS WORKING

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
 import ResultBlock from './ResultBlock'
 import { ChampionIdData, Result } from '../../d'
+import { Space } from '@mantine/core';
 
 interface SearchResultsState { }
 
 interface SearchResultsProps {
-    search_results: Result[] | null,
+    search_results: any | null,
     app_patch: string | null,
     app_championIdsToName: ChampionIdData
 }
-
-const dummyData = [
-    {
-        "profileId": "24f5cec9-6084-4cb7-bcac-fd0b03dd3beb",
-        "summonerIcon": 4413,
-        "level": 214,
-        "rank": "GRANDMASTER",
-        "topChamps": [
-            "222",
-            "17",
-            "150"
-        ],
-        "roles": ['top', 'jungle', 'mid', 'bottom', 'support'],
-        "voiceComm": false,
-        "gameModes": null,
-        'summonerName': 'summ name'
-    }
-]
 
 class SearchResults extends React.Component<SearchResultsProps, SearchResultsState> {
     constructor(props: SearchResultsProps) {
@@ -43,7 +26,20 @@ class SearchResults extends React.Component<SearchResultsProps, SearchResultsSta
     render() {
         return (
             <>
-                <ResultBlock app_championIdsToName={this.props.app_championIdsToName} app_patch={this.props.app_patch} search_result={dummyData[0]} />
+                {this.props.search_results !== null ?
+
+                    Object.keys(this.props.search_results.matches).map((key, index) => (
+                        <><ResultBlock
+                            key={index}
+                            app_championIdsToName={this.props.app_championIdsToName}
+                            app_patch={this.props.app_patch}
+                            search_result={this.props.search_results.matches[index]} />
+                            <Space h='xl'></Space>
+                        </>
+                    ))
+                    :
+                    ''
+                }
             </>
         )
     }
