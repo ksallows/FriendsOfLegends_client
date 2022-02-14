0e605d370953288c3f8ddddeb611354d06e4d04e
Katie Sallows <ktsallows@gmail.com>
2022-02-14 18:29:19 -0500

    added form for resetting passwords (admin)

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Tabs, Grid, Paper } from '@mantine/core';
+import ResetPassword from './ResetPassword';
 
 interface AdminPanelProps {
     app_sessionToken: string | null
@@ -13,7 +14,7 @@ function AdminPanel({ app_sessionToken }: AdminPanelProps) {
             <Grid.Col xs={10} md={8} lg={6} xl={6} offsetXs={1} offsetMd={2} offsetLg={3} offsetXl={3}>
                 <Paper sx={{ backgroundColor: '#1f2023', marginBottom: '2rem' }} padding='md' shadow='sm' withBorder>
                     <Tabs color='orange' active={activeTab} onTabChange={setActiveTab}>
-                        <Tabs.Tab label="Reset Password">Reset Password</Tabs.Tab>
+                        <Tabs.Tab label="Reset Password"><ResetPassword app_sessionToken={app_sessionToken} /></Tabs.Tab>
                         <Tabs.Tab label="Delete Comments">Second tab content</Tabs.Tab>
                     </Tabs>
                 </Paper>
--- src/components/admin/ResetPassword.tsx
+++ src/components/admin/ResetPassword.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Textarea, Space, Group, Button, Alert } from '@mantine/core';
+import { Title, TextInput, PasswordInput, Space, Group, Button, Divider } from '@mantine/core';
 import APIURL from '../../helpers/environment'
 
 interface ResetPasswordState {
@@ -7,6 +7,7 @@ interface ResetPasswordState {
 }
 
 interface ResetPasswordProps {
+    app_sessionToken: string | null
 }
 
 class ResetPassword extends React.Component<ResetPasswordProps, ResetPasswordState> {
@@ -19,6 +20,26 @@ class ResetPassword extends React.Component<ResetPasswordProps, ResetPasswordSta
     render() {
         return (
             <>
+                <Space h='md' />
+                <Title order={4}>Reset by Profile Id</Title>
+                <Space h='md' />
+                <Group grow>
+                    <TextInput placeholder='profile id' />
+                    <PasswordInput placeholder='new password' />
+                    <Button color='orange'>submit</Button>
+                    <Space h='md' />
+                </Group>
+                <Space h='xl' />
+                <Divider />
+                <Space h='xl' />
+                <Title order={4}>Reset by Account Id</Title>
+                <Space h='md' />
+                <Group grow>
+                    <TextInput placeholder='account id' />
+                    <PasswordInput placeholder='new password' />
+                    <Button color='orange'>submit</Button>
+                    <Space h='md' />
+                </Group>
             </>
         )
     }
